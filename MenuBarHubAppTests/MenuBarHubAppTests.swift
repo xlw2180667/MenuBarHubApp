e12fa74af9e1438ca65669d3fb81596bf5d23a84
Xie Liwei <xie.liwei@parkman.io>
2026-03-11 16:29:52 +0200

    Initial Commit

---
+++ MenuBarHubAppTests/MenuBarHubAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarHubAppTests.swift
+//  MenuBarHubAppTests
+//
+//  Created by Liwei Xie on 11.3.2026.
+//
+
+import Testing
+@testable import MenuBarHubApp
+
+struct MenuBarHubAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
